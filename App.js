ad0e1f6a2fd01f7266106f888783d0bdc334fb80
Mayck <103364944+MayckL2@users.noreply.github.com>
2023-08-21 17:20:35 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,20 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, View, Text, SafeAreaView, StatusBar } from 'react-native';
 
-export default function App() {
+import Calculadora from './components/calculadora';
+import Medida from './components/Medida';
+import Temperatura from './components/Temperatura';
+
+function App() {
+  const [tela, setTela] = useState('Calculadora');
+
+  function telaAberta(e) {
+    if (tela == 'Calculadora') {
+      return <Calculadora />;
+    } else if (tela == 'Medida') {
+      return <Medida />;
+    } else if (tela == 'Temperatura') {
+      return <Temperatura />;
+    }
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <StatusBar hidden={true}/>
+
+      <View style={styles.header}>
+        <Text style={styles.headerBtn} onPress={() => setTela('Calculadora')}>Calculadora</Text>
+        <Text style={styles.headerBtn} onPress={() => setTela('Medida')}>Medida</Text>
+        <Text style={styles.headerBtn} onPress={() => setTela('Temperatura')}>Temperatura</Text>
+      </View>
+
+      <View style={styles.tela}>{telaAberta(tela)}</View>
+    </SafeAreaView>
   );
 }
 
+export default App;
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  header: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    // paddingVertical: 50,
   },
+  tela: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100%'
+  },
+  headerBtn:{
+    backgroundColor: 'gray',
+    flexGrow: 1,
+    height: 80,
+    textAlign: 'center',
+    textAlignVertical: 'center'
+  },
+  
 });
