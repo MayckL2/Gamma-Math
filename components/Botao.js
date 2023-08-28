e815571308c02854c2ff98da515971e10d9211aa
mayckl2 <mayckluciano2@gmail.com>
2023-08-28 16:24:58 -0300

    teclado calculadora

--- components/Botao.js
+++ components/Botao.js
@@ -1,17 +1,109 @@
 import * as React from 'react';
-import { View, Text, TouchableOpacity, Button, StyleSheet } from 'react-native';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Button,
+  StyleSheet,
+  Image,
+} from 'react-native';
 
-export default function Botao({ valor, dados, estilo }) {
+export default function Botao({ valor, dados, cor, img }) {
+
+// diferencia qual a cor do botão
+  function escolhaBotao(e) {
+    if (e == 'verde') {
+      return (
+        <TouchableOpacity style={styles.botaoVerde} onPress={dados}>
+          {valor}
+          <Image source={img} />
+        </TouchableOpacity>
+      );
+    }else if(e == 'azul'){
+      return (
+        <TouchableOpacity style={styles.botaoAzul} onPress={dados}>
+          {valor}
+          <Image source={img} />
+        </TouchableOpacity>
+      );
+    }else if(e == 'cinza'){
+      return (
+        <TouchableOpacity style={styles.botaoCinza} onPress={dados}>
+          {valor}
+          <Image source={img} />
+        </TouchableOpacity>
+      );
+    }else{
+      return (
+        <TouchableOpacity style={styles.botao} onPress={dados}>
+          {valor}
+          <Image source={img} />
+        </TouchableOpacity>
+      );
+    }
+  }
+
   return (
-    <View>
-      <Text>
-        <Button
-          style={estilo}
-          title={valor}
-          accessibilityLabel={valor}
-          onPress={(dados)}
-        />
-      </Text>
-    </View>
+    // pega cor pelo props e escolhe pela funçao a cor do botão
+      escolhaBotao(cor)
+      
+    // <View>
+      // {!cor ? (
+      //   <TouchableOpacity style={styles.botao} onPress={dados}>
+      //     {valor}
+      //     <Image source={img} />
+      //   </TouchableOpacity>
+      // ) : cor == 'verde' ? (
+      //   <TouchableOpacity style={styles.botaoVerde} onPress={dados}>
+      //     {valor}
+      //     <Image source={img} />
+      //   </TouchableOpacity>
+      // ) : (
+      //   <TouchableOpacity style={styles.botao} onPress={dados}>
+      //     {valor}
+      //     <Image source={img} />
+      //   </TouchableOpacity>
+      // )}
+
+    // </View>
   );
 }
+
+const styles = StyleSheet.create({
+  botao: {
+    width: 70,
+    height: 70,
+    backgroundColor: '#1C2E48',
+    color: 'white',
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  botaoVerde: {
+    backgroundColor: '#219495',
+    width: 70,
+    height: 70,
+    color: 'white',
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  botaoCinza: {
+    backgroundColor: '#7A9FE5',
+    width: 70,
+    height: 70,
+    color: 'white',
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  botaoAzul: {
+    backgroundColor: '#7A9FE5',
+    width: 70,
+    height: 70,
+    color: 'white',
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
