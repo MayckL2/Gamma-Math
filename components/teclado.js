6237124d88bf39490a3be3cd5f322251a380a609
Breno-S <114250412+Breno-S@users.noreply.github.com>
2023-08-29 14:36:48 -0300

    visor

--- components/teclado.js
+++ components/teclado.js
@@ -19,31 +19,33 @@ export default function Teclado() {
   return (
     <View>
       <Text>SCREEN Calculadora</Text>
-      
-      <Text style={styles.visor}>{valor}</Text>
-        <Text style={styles.resposta}>{ resultado }</Text>
-      
+
+      <View style={styles.telaVisor}>
+        <Text style={styles.visor}>{valor}</Text>
+        <Text style={styles.resposta}>{resultado}</Text>
+      </View>
+
       <View style={styles.teclado}>
-        <Botao valor="C" dados={() => setValor('')} cor='cinza'/>
-        <Botao valor=")" dados={() => setValor(valor + ')')} cor='cinza'/>
-        <Botao valor="%" dados={() => setValor(valor + '%')} cor='cinza'/>
-        <Botao valor="÷" dados={() => setValor(valor + '/')} cor='cinza'/>
-        <Botao valor="9" dados={() => setValor(valor + 9)}/>
-        <Botao valor="8" dados={() => setValor(valor + 8)}/>
-        <Botao valor="7" dados={() => setValor(valor + 7)}/>
-        <Botao valor="x" dados={() => setValor(valor + '*')} cor='cinza'/>
-        <Botao valor="4" dados={() => setValor(valor + 4)}/>
-        <Botao valor="5" dados={() => setValor(valor + 5)}/>
-        <Botao valor="6" dados={() => setValor(valor + 6)}/>
-        <Botao valor="+" dados={() => setValor(valor + '+')} cor='cinza'/>
-        <Botao valor="1" dados={() => setValor(valor + 1)}/>
-        <Botao valor="2" dados={() => setValor(valor + 2)}/>
-        <Botao valor="3" dados={() => setValor(valor + 3)}/>
-        <Botao valor="-" dados={() => setValor(valor + '-')} cor='cinza'/>
-        <Botao valor="0" dados={() => setValor(valor + 0)}/>
-        <Botao valor="." dados={() => setValor(valor + '.')} cor='cinza'/>
-        <Botao dados={() => setValor(valor + 'C')} cor='verde' img={backSpace}/>
-        <Botao valor="=" dados={chamarCalcular} cor='blue'/>
+        <Botao valor="C" dados={() => setValor('')} cor='cinza' />
+        <Botao valor=")" dados={() => setValor(valor + ')')} cor='cinza' />
+        <Botao valor="%" dados={() => setValor(valor + '%')} cor='cinza' />
+        <Botao valor="÷" dados={() => setValor(valor + '/')} cor='cinza' />
+        <Botao valor="9" dados={() => setValor(valor + 9)} />
+        <Botao valor="8" dados={() => setValor(valor + 8)} />
+        <Botao valor="7" dados={() => setValor(valor + 7)} />
+        <Botao valor="x" dados={() => setValor(valor + '*')} cor='cinza' />
+        <Botao valor="4" dados={() => setValor(valor + 4)} />
+        <Botao valor="5" dados={() => setValor(valor + 5)} />
+        <Botao valor="6" dados={() => setValor(valor + 6)} />
+        <Botao valor="+" dados={() => setValor(valor + '+')} cor='cinza' />
+        <Botao valor="1" dados={() => setValor(valor + 1)} />
+        <Botao valor="2" dados={() => setValor(valor + 2)} />
+        <Botao valor="3" dados={() => setValor(valor + 3)} />
+        <Botao valor="-" dados={() => setValor(valor + '-')} cor='cinza' />
+        <Botao valor="0" dados={() => setValor(valor + 0)} />
+        <Botao valor="." dados={() => setValor(valor + '.')} cor='cinza' />
+        <Botao dados={() => setValor(valor + 'C')} cor='verde' img={backSpace} />
+        <Botao valor="=" dados={chamarCalcular} cor='blue' />
       </View>
     </View>
   );
@@ -60,9 +62,15 @@ const styles = StyleSheet.create({
     fontSize: 35
   },
   visor: {
-    color: '#fff'
+    color: 'gray',
+    textAlign: 'right',
+    fontSize: 20
   },
   resposta: {
-    color: '#fff'
+    color: '#fff',
+    textAlign: 'right',
+    fontSize: 40
+  },
+  telaVisor:{
   }
 });
