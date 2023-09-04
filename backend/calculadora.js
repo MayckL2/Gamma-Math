5d3fda7d5c475c2fc960db330502bbc0d2ddc29b
michaelsalmeida <michaellsantosalmeida@gmail.com>
2023-09-04 09:41:50 -0300

    concerto calculo de divisao mais implementacao do calculo de porcentagem simples

--- backend/calculadora.js
+++ backend/calculadora.js
@@ -1,5 +1,6 @@
 function calcular(equacao) {
     const operacao = equacao;
+    
     const sinaisOperacionais = ['+', '-', '*', '/', '.', '%'];
 
     const posicaoDaBarra = operacao.indexOf('/');
@@ -9,23 +10,69 @@ function calcular(equacao) {
             alert('Impossível realizar divisão por ZERO');
             return false;
         }
+    }
+
+    const caracterFinal = operacao[operacao.length - 1];
+    if (sinaisOperacionais.includes(caracterFinal)){
+        alert('Espressão inválida');
+        return false;
+    } 
+        // if (operacao.includes('%')){
+
+        //     var operacaoComposta = false;
+
+        //     for (var i = 0; i < operacao.length; i++){
+        //         if (sinaisOperacionais.includes(operacao[i])){
+        //             operacaoComposta = true;
+        //         }
+        //     }
+
+        //     if (operacaoComposta){
+        //         var possuiSinalAntesDaPorcentagem = false;
+        //         const posicaoDaPorcentagem = operacao.indexOf('%');
+
+        //         for (var j = posicaoDaPorcentagem; j > 0; j--){
+        //             if (sinaisOperacionais.includes(operacao[j])){
+        //                 possuiSinalAntesDaPorcentagem = true;
+        //             }
+        //         }
+
+        //         if (possuiSinalAntesDaPorcentagem){
+        //             for (k = posicaoDaPorcentagem; k > 0; k--){
+        //                 if (sinaisOperacionais.includes(operacao[k])){
+        //                     const insercaoParentesesAberto = operacao.indexOf(operacao[k]) + 1;
+        //                     const resu = operacao.slice(0, insercaoParentesesAberto) + "(" + operacao.slice(insercaoParentesesAberto);
+        //                     encontrouSinal = true;
+        //                 }
+        //             }
+
+        //         } else {
+        //             const resu = "(" + operacao;
+        //             alert(resu);
+        //         }
+
+        //     }
+        // }
+    if (operacao.includes("%")){
+        const substituicaoDaPorcentagem = operacao.replace(/%/g, "*");
+        const resul = "(" + substituicaoDaPorcentagem + ") / 100";
+
+        const resultado = eval(resul);
+
+        const retorno = resultado.toString();
+
+        return retorno;
     } else {
-        const caracterFinal = operacao[operacao.length - 1];
-        if (sinaisOperacionais.includes(caracterFinal)){
-            alert('Espressão inválida');
-            return false;
-        } else {
 
-            const resultado = eval(operacao);
-    
-            const retorno = resultado.toString();
-    
-            return retorno;
-        }
+        const resultado = eval(operacao);
 
+        const retorno = resultado.toString();
+
+        return retorno;
     }
 
 }
+
     
 function backspace (string) {
     var operacao = string;
